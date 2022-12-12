439ee4a2c24be77153411526a38bb8ffce8181d2
MagicoDave <avcasalderrey@gmail.com>
2022-12-12 18:27:43 +0100

    Primer ejercicio de conectores terminado

--- Segunda evaluacion/Tema 4/conectores/src/main/java/tema4/Application.java
+++ Segunda evaluacion/Tema 4/conectores/src/main/java/tema4/Application.java
@@ -3,6 +3,9 @@ package tema4;
 
 public class Application {
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    
+    JDBC jdbc = new JDBC();
+    jdbc.consultaAlumnosCadena("p");
+
   }
 }
--- Segunda evaluacion/Tema 4/conectores/src/main/java/tema4/JDBC.java
+++ Segunda evaluacion/Tema 4/conectores/src/main/java/tema4/JDBC.java
@@ -2,7 +2,9 @@ package tema4;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class JDBC {
     
@@ -31,6 +33,77 @@ public class JDBC {
             this.conexion.close();
         } catch (SQLException e) {
             System.out.println("Error al cerrar la conexión: " + e.getLocalizedMessage());
+        }
+    }
+
+    //Ejercicio 1
+    public void consultaAlumnosCadena (String cadena){
+        abrirConexion("add", "localhost", "root", "");
+
+        try (Statement st = this.conexion.createStatement()){
+            String query = "select * from alumnos where nombre like '%" + cadena + "%'";
+            ResultSet rs = st.executeQuery(query);
+
+            int contador = 0;
+            while (rs.next()){
+                System.out.println(rs.getInt("codigo") + ". " +  rs.getString("nombre") + " " + rs.getString("apellidos"));
+                contador++;
+            }
+            System.out.println("Tu búsqueda ha devuelto " + contador + " resultado(s)");
+        } catch (SQLException e) {
+            System.out.println("Se ha producido un error: " + e.getLocalizedMessage());
+        } finally {
+            cerrarConexion();
+        }
+    }
+
+    //Ejercicio 2
+    public void altaAlumno(String nombre, String apellidos, int altura, int aula){
+        abrirConexion("add", "localhost", "root", "");
+
+        try (Statement st = this.conexion.createStatement()){
+            
+        } catch (SQLException e) {
+            System.out.println("Se ha producido un error: " + e.getLocalizedMessage());
+        } finally {
+            cerrarConexion();
+        }
+    }
+
+    public void altaAsinatura(int cod, String nombre){
+        abrirConexion("add", "localhost", "root", "");
+
+        try (Statement st = this.conexion.createStatement()){
+            
+        } catch (SQLException e) {
+            System.out.println("Se ha producido un error: " + e.getLocalizedMessage());
+        } finally {
+            cerrarConexion();
+        }
+    }
+
+    //Ejercicio 3
+    public void bajaAlumno(int codigo){
+        abrirConexion("add", "localhost", "root", "");
+
+        try (Statement st = this.conexion.createStatement()){
+            
+        } catch (SQLException e) {
+            System.out.println("Se ha producido un error: " + e.getLocalizedMessage());
+        } finally {
+            cerrarConexion();
+        }
+    }
+
+    public void bajaAsignatura(int cod){
+        abrirConexion("add", "localhost", "root", "");
+
+        try (Statement st = this.conexion.createStatement()){
+            
+        } catch (SQLException e) {
+            System.out.println("Se ha producido un error: " + e.getLocalizedMessage());
+        } finally {
+            cerrarConexion();
         }
     }
 }
